683708896ddedb35326467cfe1b1ec2f59990250
Dmitry Salyakin <d.salyakin@gmail.com>
2022-08-20 03:06:16 +0300

    Add vimeo & lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(currentTime), 1000);
+
+function currentTime({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+player.setCurrentTime(
+  localStorage.getItem(STORAGE_KEY) ? localStorage.getItem(STORAGE_KEY) : 0
+);
